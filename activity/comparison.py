6b124f34dcab096376812f0e5f4ea9d0f7b8b77c
Camille <camilledubois1995@gmail.com>
2025-03-27 14:45:10 -0400

    implemented comparison.py and student.py

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,1 +1,6 @@
 # add your get_student_with_more_classes function here!
+def get_student_with_more_classes(student1, student2):
+
+    if len(student1.courses) > len(student2.courses):
+        return student1.name
+    return student2.name
--- activity/student.py
+++ activity/student.py
@@ -1,1 +1,16 @@
-# add your Student class here!
+class Student:
+
+    def __init__(self, name, year, courses):
+        self.name = name
+        self.year = year
+        self.courses = courses
+
+    def add_class(self, course):
+        self.courses.append(course)
+        return self.courses
+
+    def get_num_classes(self):
+        return len(self.courses)
+    
+    def summary(self):
+        return f"{self.name} is a {self.year} enrolled in {self.get_num_classes()} classes."
