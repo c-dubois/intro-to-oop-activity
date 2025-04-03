1f47ec64b56c8a67b6ca6f5909b8e91f14c2e5d6
Camille <camilledubois1995@gmail.com>
2025-04-03 17:21:03 -0400

    implemented student class and comparison function

--- activity/comparison.py
+++ activity/comparison.py
@@ -1,6 +1,13 @@
-# add your get_student_with_more_classes function here!
+from student import Student
 def get_student_with_more_classes(student1, student2):
 
-    if len(student1.courses) > len(student2.courses):
+    if student1.get_num_classes() > student2.get_num_classes():
         return student1.name
     return student2.name
+
+# example
+
+student1 = Student("Alice", "Sophomore", ["Math", "Science"])
+student2 = Student("Bob", "Freshman", ["History"])
+
+print(get_student_with_more_classes(student1, student2))
